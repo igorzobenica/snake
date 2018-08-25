28e2c814f202769f439c953b6e4ade5a8b9a9999
Asus <Asus@DESKTOP-TJEJDMS>
2018-08-25 14:48:34 +0200

    implementing enum and switch in board

--- Engine/Board.cpp
+++ Engine/Board.cpp
@@ -36,18 +36,18 @@ bool Board::IsInsideBoard( const Location & loc ) const
 		loc.y >= 0 && loc.y < height;
 }
 
-int Board::GetContents(const Location & loc) const
+Board::CellContents Board::GetContents(const Location & loc) const
 {
 	return contents[loc.y * width + loc.x];
 }
 
 void Board::ConsumeContents(const Location & loc)
 {
-	assert(GetContents(loc) == 2 || GetContents(loc) == 3);
-	contents[loc.y * width + loc.x] = 0;
+	assert(GetContents(loc) == CellContents::Food || GetContents(loc) == CellContents::Poison);
+	contents[loc.y * width + loc.x] = CellContents::Empty;
 }
 
-void Board::SpawnContents(std::mt19937 & rng, const Snake & snake, int contentsType)
+void Board::SpawnContents(std::mt19937 & rng, const Snake & snake, CellContents contentsType)
 {
 	std::uniform_int_distribution<int> xDist(0, GetGridWidth() - 1);
 	std::uniform_int_distribution<int> yDist(0, GetGridHeight() - 1);
@@ -57,7 +57,7 @@ void Board::SpawnContents(std::mt19937 & rng, const Snake & snake, int contentsT
 	{
 		newLoc.x = xDist(rng);
 		newLoc.y = yDist(rng);
-	} while (snake.IsInTile(newLoc) || GetContents(newLoc) != 0);
+	} while (snake.IsInTile(newLoc) || GetContents(newLoc) != CellContents::Empty);
 
 	contents[newLoc.y * width + newLoc.x] = contentsType;
 }
@@ -85,18 +85,19 @@ void Board::DrawCells()
 	{
 		for (int x = 0; x < width; x++)
 		{
-			const int contents = GetContents({ x,y });
-			if (contents == 1)
+			switch (GetContents({ x,y }))
 			{
+			case CellContents::Obstacle:
 				DrawCell({ x,y }, obstacleColor);
-			}
-			else if (contents == 2)
-			{
+				break;
+			case CellContents::Food:
 				DrawCell({ x,y }, foodColor);
-			}
-			else if (contents == 3)
-			{
+				break;
+			case CellContents::Poison:
 				DrawCell({ x,y }, poisonColor);
+				break;
+			default:
+				break;
 			}
 		}
 
--- Engine/Board.h
+++ Engine/Board.h
@@ -7,21 +7,29 @@
 class Board
 {
 public:
+	enum class CellContents
+	{
+		Empty,
+		Obstacle,
+		Food,
+		Poison
+	};
+public:
 	Board( Graphics& gfx );
 	void DrawCell( const Location& loc,Color c );
 	int GetGridWidth() const;
 	int GetGridHeight() const;
 	bool IsInsideBoard( const Location& loc ) const;
-	int GetContents(const Location& loc) const;
+	CellContents GetContents(const Location& loc) const;
 	void ConsumeContents(const Location& loc);
-	void SpawnContents(std::mt19937& rng, const class Snake& snake, int contentsType);
+	void SpawnContents(std::mt19937& rng, const class Snake& snake, CellContents contents);
 	void DrawBorder();
 	void DrawCells();
 private:
 	static constexpr Color borderColor = Colors::Blue;
 	static constexpr Color obstacleColor = Colors::Gray;
 	static constexpr Color foodColor = Colors::Red;
-	static constexpr Color poisonColor = Colors::Magenta;
+	static constexpr Color poisonColor = { 64,8,64 };
 	static constexpr int dimension = 20;
 	static constexpr int cellPadding = 1;
 	static constexpr int width = 32;
@@ -30,7 +38,6 @@ private:
 	static constexpr int borderPadding = 2;
 	static constexpr int x = 70;
 	static constexpr int y = 50;
-	// 0:empty 1:obstacle 2:food 3:poison-speedup
-	int contents[width * height] = { 0 };
+	CellContents contents[width * height] = { CellContents::Empty };
 	Graphics& gfx;
 };
